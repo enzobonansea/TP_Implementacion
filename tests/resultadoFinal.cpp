693313c095e56cd864c9ff553b63a642aa700908
enzo <ebonansea@dc.uba.ar>
2017-11-13 05:01:31 -0300

    Correccion resultadoFinal() y su test

---
+++ tests/resultadoFinal.cpp
@@ -0,0 +1,23 @@
+#include "../Funciones_TPI.h"
+#include "../ejercicios.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(resultadoFinal, salaCompleta){
+cout << "El F1-score promedio de la sala es: ";
+int frecuencia, profundidad, duracion = 0;
+vector<string> archivos = {"datos/spkr0.dat",
+                           "datos/spkr1.dat",
+                           "datos/spkr2.dat",
+                           "datos/spkr3.dat",
+                           "datos/spkr4.dat",
+                           "datos/spkr5.dat",
+                           "datos/pzm.dat"};
+sala reunion = cargarSalaAudio(archivos, frecuencia, profundidad, duracion);
+int umbral = 4000;
+cout << resultadoFinal(reunion, frecuencia, profundidad, umbral);
+EXPECT_TRUE(true);
+/* este test siempre va a ser pasado, pero lo que me interesa a mi es el valor del
+ * estadistico que imprimo por pantalla */
+}
